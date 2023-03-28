6260fc95da0c7946b3b23d080f92524eac570021
Manuel Echavarria <mechavarria@aldereca.com>
2023-03-28 16:30:34 -0400

    First commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Home">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Home
+            </h2>
+        </template>
+
+    </AppLayout>
+</template>
